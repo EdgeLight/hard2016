68415224a3a2bc135748c74711a3a0c0ebca16af
HHJ1501 <572674605@qq.com>
2016-05-18 12:27:49 +0800

    ajax

--- js/game.js
+++ js/game.js
@@ -68,14 +68,23 @@ function ajax_start(){
 				 overtime:overtime, //overtime为0时表示超时
 				 answer:answer //选择的选项，'start'表示开始答题(虽然不一定接start，还是放在这里)
 	    },
-	    dataType: 'text',
-	    success:function(jsondata) {
-				//   status  = jsondata.step;    //start表示游戏开始，over表示游戏结束，move表示继续前进，stay表示停留在原答题点再答一次
-			 	// 	q_num   = jsondata.question;//题目号(下一次的题号！！)
-			 	// 	score   = jsondata.score;//最终成绩，也是当前题目数
-				// 	update_share();//每次请求后更新分享内容
-			    // ajax_over();   //ajax返回后的函数（纯前端）
-				console.log(jsondata);
+	    // dataType: 'json',
+	    // success:function(jsondata) {
+		// 		  status  = jsondata.step;    //start表示游戏开始，over表示游戏结束，move表示继续前进，stay表示停留在原答题点再答一次
+		// 	 		q_num   = jsondata.question;//题目号(下一次的题号！！)
+		// 	 		score   = jsondata.score;//最终成绩，也是当前题目数
+		// 			update_share();//每次请求后更新分享内容
+		// 	    ajax_over();   //ajax返回后的函数（纯前端）
+		// 		},
+		dataType: 'text',
+	    success:function(data) {
+			console.log(data);
+			var jsondata = eval(data);
+				  status  = jsondata.step;    //start表示游戏开始，over表示游戏结束，move表示继续前进，stay表示停留在原答题点再答一次
+			 		q_num   = jsondata.question;//题目号(下一次的题号！！)
+			 		score   = jsondata.score;//最终成绩，也是当前题目数
+					update_share();//每次请求后更新分享内容
+			    ajax_over();   //ajax返回后的函数（纯前端）
 				},
 	    error:function(a,b,c){
 			console.log(a,b,c);
