6afea424f3798fa9161af5da7cf0b065b37fd79e
EdgeLight <2467138649@qq.com>
2016-05-19 10:01:13 +0800

    重新更改路径
    添加./

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ var news = {
 	"Title" : "毕业之旅",
 	"Description" : "本宝宝不服，竟然才华工幼儿园毕业？戳链接来毕业",
 	"Url" : "",
-	"PicUrl" : "resource/youeryuan.png"
+	"PicUrl" : "./resource/youeryuan.png"
 };
 if (area == 'north') {
 	news['Url'] = 'http://graduation.100steps.net/alumni2016/index.php/Index/game?location=north';
@@ -47,16 +47,16 @@ function update_share() {
 	}
 	if (score <= 5) {
 		news['Description'] = "本宝宝不服，机智如我竟然才华工幼儿园毕业？";
-		news['PicUrl'] = "resource/youeryuan.png";
+		news['PicUrl'] = "./resource/youeryuan.png";
   }else if (score >= 6 && score <= 9) {
 		news['Description'] = "读了十几年书居然才是华工小学生？不服！";
-		news['PicUrl'] = "resource/xiaoxue.png";
+		news['PicUrl'] = "./resource/xiaoxue.png";
   }else if (score >= 10 && score <= 12) {
 		news['Description'] = "学富五车的我还是嫩嫩的华工高中生！";
-		news['PicUrl'] = "resource/zhongxue.png";
+		news['PicUrl'] = "./resource/zhongxue.png";
   }else if (score >= 13) {
 		news['Description'] = "本宝宝可是名正言顺从华工毕业！";
-		news['PicUrl'] = "resource/daxue.png";
+		news['PicUrl'] = "./resource/daxue.png";
   }
 		//分享给朋友
 		wx.onMenuShareAppMessage({
@@ -437,15 +437,15 @@ point[24] = {
 
 
 $(document).ready(function() {
-	loadImage('resource/one.gif', function(){              //调用图片预加载函数
+	loadImage('./resource/one.gif', function(){              //调用图片预加载函数
 	});
-	loadImage('resource/two.gif', function(){
+	loadImage('./resource/two.gif', function(){
 	});
-	loadImage('resource/three.gif', function(){
+	loadImage('./resource/three.gif', function(){
 	});
-	loadImage('resource/four.gif', function(){
+	loadImage('./resource/four.gif', function(){
 	});
-	loadImage('resource/five.gif', function(){
+	loadImage('./resource/five.gif', function(){
 	});
 
 	$(".startBtn").click(function() {                                     //开始按钮点击后（传一次ajax）
